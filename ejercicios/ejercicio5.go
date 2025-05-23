0638038527a82c2c8f7c13fd564e0d462077a846
Leonardo <leodocimo@gmail.com>
2025-05-22 21:32:17 -0300

    Eliminar metodo de prueba

--- ejercicios/ejercicio5.go
+++ ejercicios/ejercicio5.go
@@ -3,13 +3,5 @@ package ejercicios
 var billetes = []int{10000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1}
 
 func Cambiar(cantidad int) map[int]int {
-	cambio := make(map[int]int)
-	for _, denominacion := range billetes {
-		if cantidad >= denominacion {
-			cantidad_billetes := cantidad / denominacion
-			cambio[denominacion] = cantidad_billetes
-			cantidad = cantidad % denominacion
-		}
-	}
-	return cambio
+	return nil
 }
